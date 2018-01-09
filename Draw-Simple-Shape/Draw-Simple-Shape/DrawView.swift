11d56fd9e66179137441d71a05ee44393abb2008
Tanjim Hossain <tanjim@revesoft.com>
2018-01-09 10:28:40 +0600

    A rectangle is drawn using UIBezierPath

--- Draw-Simple-Shape/Draw-Simple-Shape/DrawView.swift
+++ Draw-Simple-Shape/Draw-Simple-Shape/DrawView.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class DrawView: UIView {
+    
+    var path : UIBezierPath!
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = UIColor.lightGray
+        self.backgroundColor = UIColor.white
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -20,7 +22,26 @@ class DrawView: UIView {
     }
     override func draw(_ rect: CGRect) {
         // Drawing code
+        createRectangle()
     }
     
+    //MARK: Private Methods
+    
+    func createRectangle() {
+        
+        path = UIBezierPath()
+        path.move(to: CGPoint(x: 10, y: 10))
+        path.addLine(to: CGPoint(x: self.frame.size.width - 10, y: 10))
+        path.addLine(to: CGPoint(x: self.frame.size.width - 10, y: self.frame.size.height - 10))
+        path.addLine(to: CGPoint(x: 10, y: self.frame.size.height - 10))
+        path.close()
+        
+        UIColor.orange.setFill()
+        path.fill()
+        
+        UIColor.red.setStroke()
+        path.stroke()
+        
+    }
 
 }
