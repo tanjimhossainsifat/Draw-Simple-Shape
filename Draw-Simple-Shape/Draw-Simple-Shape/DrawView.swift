243378fde15ebdd3b0a551ff2c4f867c1e367568
Tanjim Hossain <tanjim@revesoft.com>
2018-01-09 10:36:37 +0600

    Triangle creation method added

--- Draw-Simple-Shape/Draw-Simple-Shape/DrawView.swift
+++ Draw-Simple-Shape/Draw-Simple-Shape/DrawView.swift
@@ -22,7 +22,9 @@ class DrawView: UIView {
     }
     override func draw(_ rect: CGRect) {
         // Drawing code
-        createRectangle()
+        //createRectangle()
+        createTriangle()
+        
     }
     
     //MARK: Private Methods
@@ -30,13 +32,29 @@ class DrawView: UIView {
     func createRectangle() {
         
         path = UIBezierPath()
-        path.move(to: CGPoint(x: 10, y: 10))
-        path.addLine(to: CGPoint(x: self.frame.size.width - 10, y: 10))
-        path.addLine(to: CGPoint(x: self.frame.size.width - 10, y: self.frame.size.height - 10))
-        path.addLine(to: CGPoint(x: 10, y: self.frame.size.height - 10))
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: self.frame.size.width , y: 0))
+        path.addLine(to: CGPoint(x: self.frame.size.width, y: self.frame.size.height))
+        path.addLine(to: CGPoint(x: 0, y: self.frame.size.height))
         path.close()
         
         UIColor.orange.setFill()
+        path.fill()
+        
+        UIColor.red.setStroke()
+        path.stroke()
+        
+    }
+    
+    func createTriangle() {
+        
+        path = UIBezierPath()
+        path.move(to: CGPoint(x: self.frame.size.width/2, y:0))
+        path.addLine(to: CGPoint(x: self.frame.size.width, y: self.frame.size.height))
+        path.addLine(to: CGPoint(x: 0, y: self.frame.size.height))
+        path.close()
+        
+        UIColor.blue.setFill()
         path.fill()
         
         UIColor.red.setStroke()
